ffdb47a0bca7543e235b6ca17b1f92f167f796bd
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-12-16 11:50:35 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Gabriel Akinyosoye on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
